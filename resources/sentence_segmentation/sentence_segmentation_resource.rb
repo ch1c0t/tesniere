ee451d23f97f74c65340f840b8e0d827c7aaaa81
Anatoly Chernow <ch1c0t@yahoo.com>
2013-08-10 09:29:58 +0000

    Thou shall be a poet

--- resources/sentence_segmentation/sentence_segmentation_resource.rb
+++ resources/sentence_segmentation/sentence_segmentation_resource.rb
@@ -2,7 +2,7 @@ class SentenceSegmentationResource < Sinatra::Base
   Meta = Redis.new :port => 6382, :db => 2
 
   post '/segment' do
-    params = Oj.load(request.body.read)
+    params = Oj.load request.body.read
     params = params.delete_if { |_, value| value.empty? }
     params['paragraph_id'] = Meta.incr 'paragraph-ids_counter'
 
