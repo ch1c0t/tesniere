754b6ebb8c01b95d0688b5a1075a22cf36219344
Anatoly Chernow <ch1c0t@yahoo.com>
2013-08-11 09:44:34 +0000

    Add predicators

--- resources/sentence_collection/sentence_collection_resource.rb
+++ resources/sentence_collection/sentence_collection_resource.rb
@@ -1,12 +1,21 @@
 class SentenceCollectionResource < Sinatra::Base
   R = Redis.new :port => 6382
   Meta = Redis.new :port => 6382, :db => 1
+  Predicators = Redis.new :port => 6382, :db => 3
 
   post '/sentence_collection/new' do
     sentences = Oj.load request.body.read
 
     sentences.each do |sentence|
       id = Meta.incr 'sentence-ids_counter'
+
+      if sentence['predicators']
+        string = sentence['predicators']
+        string.split(';').each do |predicator|
+          Predicators.sadd predicator, id
+        end
+      end
+
       sentence.each do |key, value|
         R.hset id, key, value
       end
