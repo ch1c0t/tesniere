89c0802644f136e85ffc2228c5335eec46fbca2c
Anatoly Chernow <ch1c0t@yahoo.com>
2013-08-30 04:00:54 +0000

    Install to hidden directory in order to avoid ambiguity

--- config.rb
+++ config.rb
@@ -1,7 +1,8 @@
 require 'pathname'
+require 'redis'
 
 module Tesniere
-  ROOT_PATH = Pathname.new('~/tesniere').expand_path
+  ROOT_PATH = Pathname.new('~/.tesniere').expand_path
   DB_PATH   = ROOT_PATH + 'db'
 
   port = 7777
